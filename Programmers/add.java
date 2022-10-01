27a55023dde4895a370419d00efc8e8ca10a6761
Se-Yeon99 <84967078+Se-Yeon99@users.noreply.github.com>
2022-10-01 23:59:42 +0900

    Update add.java

--- Programmers/add.java
+++ Programmers/add.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
+        int answer= 0;
         
-        answer =  num1 % num2 ;
-        return answer * 1000;
+        double result = ((double)num1 / num2) * 1000;
+        answer = (int)result;
+        
+        return answer;
     }
 }
