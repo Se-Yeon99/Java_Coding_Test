133063e9b37d2884ae73935458f62c22e52cd276
[Se-Yeon99] <[hsy_0204@naver.com]>
2022-10-10 01:08:44 +0900

    평균 구하기

--- Programmers/코딩테스트 입문/Average.java
+++ Programmers/코딩테스트 입문/Average.java
@@ -1,1 +1,13 @@
-
+class Average {
+    public double Average(int[] arr) {
+        double answer = 0;
+        double result = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            result += arr[i];
+            answer = result / arr.length;
+        }
+        
+        return answer;
+    }
+}
