99863e8b6c769e8735bb6f9548bf017009fff4cd
Se-Yeon99 <84967078+Se-Yeon99@users.noreply.github.com>
2022-11-08 21:18:52 +0900

    나머지가 1이 되는 수 찾기

--- Programmers/코딩테스트 Level 01/Remainder.java
+++ Programmers/코딩테스트 Level 01/Remainder.java
@@ -1,1 +1,12 @@
-
+class Remainder {
+    public int Remainder(int n) {
+        int x = 0;
+      
+        for( x=1; x < n; x++){
+            if(n % x == 1){
+                return x ;
+            }
+        }
+        return x;
+    }
+}
