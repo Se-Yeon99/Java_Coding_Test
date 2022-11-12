a6f8ee2644ebf01d4a31be867d750478dc9cbb9a
Se-Yeon99 <84967078+Se-Yeon99@users.noreply.github.com>
2022-11-12 22:26:22 +0900

    서울에서 김서방 찾기

--- Programmers/코딩테스트 Level 01/Kim.java
+++ Programmers/코딩테스트 Level 01/Kim.java
@@ -1,20 +1,13 @@
 class Solution {
     public String solution(String[] seoul) {
-        int x = 0;
-       // String answer = "김서방은 "+ x + "에 있다" ;
-        String a = "";
+        String answer="";
         
-        if(true){
-            for(int i = 0; i < seoul.length; i++){
-                if(seoul[i] == "Kim"){
-                     x += i;
-                    String answer = "김서방은 "+ x + "에 있다" ;
-                    return answer;
-                }else{
-                //   return "못 찾았습니다.";
+        for(int i = 0; i < seoul.length; i++){
+             if(seoul[i].equals("Kim")){
+                   int x = i;
+                   return answer = "김서방은 " + x + "에 있다";
                 }
             }
-        }
-        return a;
+        return answer;
     }
 }
