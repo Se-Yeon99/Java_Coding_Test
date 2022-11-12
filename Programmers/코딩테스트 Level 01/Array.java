1358463afc835e7377256b64471a956ec9657756
Se-Yeon99 <84967078+Se-Yeon99@users.noreply.github.com>
2022-11-12 23:01:56 +0900

    자연수 뒤집어 배열로 만들기

--- Programmers/코딩테스트 Level 01/Array.java
+++ Programmers/코딩테스트 Level 01/Array.java
@@ -1,11 +1,15 @@
- public int[] Array(long n) {
-		 String s =""+n; //String에 n 추가
-		 int[] answer = new int[s.length()];//입력받은 숫자의 길이만큼
-		 int cnt=0;
-		 while(n>0) {
-			 answer[cnt]=(int)(n%10);//12345 -> 나머지 5 -> 4 -> 3 -> 2 ->1
-			 n/=10; // n=1234 -> 123 -> -> 12 -> 1
-			 cnt++;
-		 }
-		 return answer;
-	 }
+class Array {
+    public int[] Array(long n) {
+        String a = "" + n;
+        int[] answer = new int[a.length()];
+        int num = 0;
+        
+        while(n > 0){
+            answer[num] = (int)(n % 10);
+            n /= 10;
+            
+            num++;
+        }
+        return answer;
+    }
+}
