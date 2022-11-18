cbb90c8873ea9c1ab4e1b8ee932de7a4ca67d907
Se-Yeon99 <84967078+Se-Yeon99@users.noreply.github.com>
2022-11-18 23:08:22 +0900

    수박수박수박수박수박수?

---
+++ Programmers/코딩테스트 Level 01/WaterMelon.java
@@ -0,0 +1,14 @@
+class WaterMelon {
+    public String WaterMelon(int n) {
+        String answer = "";
+        
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0){
+                answer += "수";
+            }else {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
